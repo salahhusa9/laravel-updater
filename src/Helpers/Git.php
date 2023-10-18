b642c34e1c6aefc71e72b5d8edcb3e46ab3f4de8
salahhusa9 <salahhusa9@gmail.com>
2023-10-18 20:13:26 +0100

    try catch in prosses

--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -10,58 +10,80 @@ class Git
     public static function getCurrentCommit()
     {
         $process = new Process([self::gitPath(), 'log', '--pretty="%h"', '-n1', 'HEAD']);
-        $process->run();
 
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function getCurrentBranch()
     {
         $process = new Process([self::gitPath(), 'rev-parse', '--abbrev-ref', 'HEAD']);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function getCurrentTag()
     {
         $process = new Process([self::gitPath(), 'describe', '--tags', '--abbrev=0']);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function auth()
     {
         // git remote set-url origin https://username:token@github.com/your/repository.git
         $process = new Process([self::gitPath(), 'remote', 'set-url', 'origin', 'https://' . config('updater.github_username') . ':' . config('updater.github_token') . '@github.com/' . config('updater.github_username') . '/' . config('updater.github_repository') . '.git']);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function pull()
     {
         $process = new Process([self::gitPath(), 'pull']);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function checkout($branch)
     {
         $process = new Process([self::gitPath(), 'checkout', $branch]);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function fetch()
     {
         $process = new Process([self::gitPath(), 'fetch']);
-        $process->run();
-
-        return trim($process->getOutput());
+        try {
+            $process->mustRun();
+            return trim($process->getOutput());
+        } catch (\Throwable $th) {
+            return new \Exception($th->getMessage());
+        }
     }
 
     public static function gitPath()
