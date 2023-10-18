ee11855e54828cbe5341992a043fce7cac311d88
salahhusa9 <salahhusa9@gmail.com>
2023-10-18 20:10:44 +0100

    add getPath()

--- config/updater.php
+++ config/updater.php
@@ -2,8 +2,12 @@
 
 // config for Salahhusa9/Updater
 return [
-    'github_token' => env('GITHUB_TOKEN'),
+
+    'git_path' => null,
+
     'repository_source' => \Salahhusa9\Updater\RepositorySource\GithubRepository::class,
+    'github_token' => env('GITHUB_TOKEN'),
     'github_username' => env('GITHUB_USERNAME'),
     'github_repository' => env('GITHUB_REPOSITORY'),
+
 ];
--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -2,13 +2,14 @@
 
 namespace Salahhusa9\Updater\Helpers;
 
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\Process\Process;
 
 class Git
 {
     public static function getCurrentCommit()
     {
-        $process = new Process(['git', 'log', '--pretty="%h"', '-n1', 'HEAD']);
+        $process = new Process([self::getPath(), 'log', '--pretty="%h"', '-n1', 'HEAD']);
         $process->run();
 
         return trim($process->getOutput());
@@ -16,7 +17,7 @@ class Git
 
     public static function getCurrentBranch()
     {
-        $process = new Process(['git', 'rev-parse', '--abbrev-ref', 'HEAD']);
+        $process = new Process([self::getPath(), 'rev-parse', '--abbrev-ref', 'HEAD']);
         $process->run();
 
         return trim($process->getOutput());
@@ -24,7 +25,7 @@ class Git
 
     public static function getCurrentTag()
     {
-        $process = new Process(['git', 'describe', '--tags', '--abbrev=0']);
+        $process = new Process([self::getPath(), 'describe', '--tags', '--abbrev=0']);
         $process->run();
 
         return trim($process->getOutput());
@@ -33,7 +34,7 @@ class Git
     public static function auth()
     {
         // git remote set-url origin https://username:token@github.com/your/repository.git
-        $process = new Process(['git', 'remote', 'set-url', 'origin', 'https://'.config('updater.github_username').':'.config('updater.github_token').'@github.com/'.config('updater.github_username').'/'.config('updater.github_repository').'.git']);
+        $process = new Process([self::getPath(), 'remote', 'set-url', 'origin', 'https://' . config('updater.github_username') . ':' . config('updater.github_token') . '@github.com/' . config('updater.github_username') . '/' . config('updater.github_repository') . '.git']);
         $process->run();
 
         return trim($process->getOutput());
@@ -41,7 +42,7 @@ class Git
 
     public static function pull()
     {
-        $process = new Process(['git', 'pull']);
+        $process = new Process([self::getPath(), 'pull']);
         $process->run();
 
         return trim($process->getOutput());
@@ -49,7 +50,7 @@ class Git
 
     public static function checkout($branch)
     {
-        $process = new Process(['git', 'checkout', $branch]);
+        $process = new Process([self::getPath(), 'checkout', $branch]);
         $process->run();
 
         return trim($process->getOutput());
@@ -57,9 +58,23 @@ class Git
 
     public static function fetch()
     {
-        $process = new Process(['git', 'fetch']);
+        $process = new Process([self::getPath(), 'fetch']);
         $process->run();
 
         return trim($process->getOutput());
+    }
+
+    public static function getPath()
+    {
+        $gitPath = config('updater.git_path');
+
+        if (!$gitPath) {
+            $gitPath = Cache::rememberForever('git_path', function () {
+                $executableFinder = new \Symfony\Component\Process\ExecutableFinder();
+                return $executableFinder->find('git');
+            });
+        }
+
+        return $gitPath;
     }
 }
