a99c496bddfc3a1d22ab4184ba8421c40395a712
salahhusa9 <salahhusa9@gmail.com>
2023-10-18 20:11:11 +0100

    *gitPath

--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -9,7 +9,7 @@ class Git
 {
     public static function getCurrentCommit()
     {
-        $process = new Process([self::getPath(), 'log', '--pretty="%h"', '-n1', 'HEAD']);
+        $process = new Process([self::gitPath(), 'log', '--pretty="%h"', '-n1', 'HEAD']);
         $process->run();
 
         return trim($process->getOutput());
@@ -17,7 +17,7 @@ class Git
 
     public static function getCurrentBranch()
     {
-        $process = new Process([self::getPath(), 'rev-parse', '--abbrev-ref', 'HEAD']);
+        $process = new Process([self::gitPath(), 'rev-parse', '--abbrev-ref', 'HEAD']);
         $process->run();
 
         return trim($process->getOutput());
@@ -25,7 +25,7 @@ class Git
 
     public static function getCurrentTag()
     {
-        $process = new Process([self::getPath(), 'describe', '--tags', '--abbrev=0']);
+        $process = new Process([self::gitPath(), 'describe', '--tags', '--abbrev=0']);
         $process->run();
 
         return trim($process->getOutput());
@@ -34,7 +34,7 @@ class Git
     public static function auth()
     {
         // git remote set-url origin https://username:token@github.com/your/repository.git
-        $process = new Process([self::getPath(), 'remote', 'set-url', 'origin', 'https://' . config('updater.github_username') . ':' . config('updater.github_token') . '@github.com/' . config('updater.github_username') . '/' . config('updater.github_repository') . '.git']);
+        $process = new Process([self::gitPath(), 'remote', 'set-url', 'origin', 'https://' . config('updater.github_username') . ':' . config('updater.github_token') . '@github.com/' . config('updater.github_username') . '/' . config('updater.github_repository') . '.git']);
         $process->run();
 
         return trim($process->getOutput());
@@ -42,7 +42,7 @@ class Git
 
     public static function pull()
     {
-        $process = new Process([self::getPath(), 'pull']);
+        $process = new Process([self::gitPath(), 'pull']);
         $process->run();
 
         return trim($process->getOutput());
@@ -50,7 +50,7 @@ class Git
 
     public static function checkout($branch)
     {
-        $process = new Process([self::getPath(), 'checkout', $branch]);
+        $process = new Process([self::gitPath(), 'checkout', $branch]);
         $process->run();
 
         return trim($process->getOutput());
@@ -58,13 +58,13 @@ class Git
 
     public static function fetch()
     {
-        $process = new Process([self::getPath(), 'fetch']);
+        $process = new Process([self::gitPath(), 'fetch']);
         $process->run();
 
         return trim($process->getOutput());
     }
 
-    public static function getPath()
+    public static function gitPath()
     {
         $gitPath = config('updater.git_path');
 
