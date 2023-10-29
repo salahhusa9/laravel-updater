e43ada21172d6baef470fdd8f888fb9b515366b8
salahhusa9 <salahhusa9@gmail.com>
2023-10-29 12:30:42 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -4,16 +4,11 @@ const withMarkdoc = require('@markdoc/next.js')
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     scrollRestoration: true,
   },
-  // change base path of assets in call: i host in github pages in salahhusa9.github.io/laravel-updater but assets are in salahhusa9.github.io/laravel-updater/_next
   assetPrefix: process.env.NODE_ENV === 'production' ? '/laravel-updater/' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/laravel-updater' : '',
-
 }
 
 module.exports = withMarkdoc()(nextConfig)
