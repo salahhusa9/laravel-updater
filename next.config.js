a0afcb7dc0132f37fb9547a15db2308fc596b6d1
salahhusa9 <salahhusa9@gmail.com>
2023-10-29 12:20:43 +0100

    // change base path of assets in call: i host in github pages in salahhusa9.github.io/laravel-updater but assets are in salahhusa9.github.io/laravel-updater/_next

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  // change base path of assets in call: i host in github pages in salahhusa9.github.io/laravel-updater but assets are in salahhusa9.github.io/laravel-updater/_next
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/laravel-updater/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/laravel-updater' : '',
+
 }
 
 module.exports = withMarkdoc()(nextConfig)
