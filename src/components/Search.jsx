3a870880999b60a2b1fd3b2626a0af86b38e4b6c
salahhusa9 <salahhusa9@gmail.com>
2023-10-31 18:04:12 +0100

    docsearch

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,9 +5,9 @@ import Router from 'next/router'
 import { DocSearchModal, useDocSearchKeyboardEvents } from '@docsearch/react'
 
 const docSearchConfig = {
-  appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID,
-  apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY,
-  indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME,
+  appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID ?? '2IKUIHFDA9',
+  apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY ?? '5e2d5accb7736426b69889293aab263e',
+  indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME ?? 'docsearch',
   searchParameters: {
     facetFilters: ['language:en', 'version:1.0.0'],
   },
