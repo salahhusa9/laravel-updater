76a02a43192819102997a7ee1b7d450c59d67218
salahhusa9 <salahhusa9@gmail.com>
2023-11-11 11:44:49 +0100

    wip

--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -20,7 +20,7 @@ class Git
         $process = Process::run(self::gitPath().' log --pretty="%h" -n1 HEAD');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -38,7 +38,7 @@ class Git
         $process = Process::run(self::gitPath().' rev-parse --abbrev-ref HEAD');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -56,7 +56,7 @@ class Git
         $process = Process::run(self::gitPath().' describe --tags --abbrev=0');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -74,7 +74,7 @@ class Git
         $process = Process::run(self::gitPath().' remote set-url origin https://'.config('updater.github_username').':'.config('updater.github_token').'@github.com/'.config('updater.github_username').'/'.config('updater.github_repository').'.git');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -92,7 +92,7 @@ class Git
         $process = Process::run(self::gitPath().' pull');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -111,7 +111,7 @@ class Git
         $process = Process::run(self::gitPath().' checkout '.$branch);
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
@@ -129,7 +129,7 @@ class Git
         $process = Process::run(self::gitPath().' fetch');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: \n'.$process->command().' with message: \n'.$process->errorOutput());
+            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
         }
 
         return trim($process->output());
