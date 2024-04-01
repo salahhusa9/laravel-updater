4b96f2c0e8657a280da550e6ad643fd292c84517
salahhusa9 <salahhusa9@gmail.com>
2024-04-01 08:44:51 +0000

    wip

--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -17,10 +17,10 @@ class Git
      */
     public static function getCurrentCommit()
     {
-        $process = Process::run(self::gitPath().' log --pretty="%h" -n1 HEAD');
+        $process = Process::run(self::gitPath() . ' log --pretty="%h" -n1 HEAD');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -35,10 +35,10 @@ class Git
      */
     public static function getCurrentBranch()
     {
-        $process = Process::run(self::gitPath().' rev-parse --abbrev-ref HEAD');
+        $process = Process::run(self::gitPath() . ' rev-parse --abbrev-ref HEAD');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -53,10 +53,10 @@ class Git
      */
     public static function getCurrentTag()
     {
-        $process = Process::run(self::gitPath().' describe --tags --abbrev=0');
+        $process = Process::run(self::gitPath() . ' describe --tags --abbrev=0');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -71,10 +71,10 @@ class Git
      */
     public static function auth()
     {
-        $process = Process::run(self::gitPath().' remote set-url origin https://'.config('updater.github_username').':'.config('updater.github_token').'@github.com/'.config('updater.github_username').'/'.config('updater.github_repository').'.git');
+        $process = Process::run(self::gitPath() . ' remote set-url origin https://' . config('updater.github_username') . ':' . config('updater.github_token') . '@github.com/' . config('updater.github_username') . '/' . config('updater.github_repository') . '.git');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -89,10 +89,10 @@ class Git
      */
     public static function pull()
     {
-        $process = Process::run(self::gitPath().' pull');
+        $process = Process::run(self::gitPath() . ' pull');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -108,10 +108,10 @@ class Git
      */
     public static function checkout($branch)
     {
-        $process = Process::run(self::gitPath().' checkout '.$branch);
+        $process = Process::run(self::gitPath() . ' checkout ' . $branch);
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -126,10 +126,10 @@ class Git
      */
     public static function fetch()
     {
-        $process = Process::run(self::gitPath().' fetch');
+        $process = Process::run(self::gitPath() . ' fetch');
 
         if ($process->failed()) {
-            throw new GitFailedException('this command failed: '.$process->command().' with message: '.$process->errorOutput());
+            throw new GitFailedException('this command failed: ' . $process->command() . ' with message: ' . $process->errorOutput());
         }
 
         return trim($process->output());
@@ -144,14 +144,14 @@ class Git
     {
         $gitPath = config('updater.git_path');
 
-        if (! $gitPath) {
+        if (!$gitPath) {
             $gitPath = Cache::rememberForever('git_path', function () {
                 $executableFinder = new \Symfony\Component\Process\ExecutableFinder();
 
                 $gitPath = $executableFinder->find('git');
 
                 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                    $gitPath = '"'.$gitPath.'"';
+                    $gitPath = '"' . $gitPath . '"';
                 }
 
                 return $gitPath;
