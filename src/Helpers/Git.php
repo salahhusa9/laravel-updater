c75212c608b61d9c12719203cee2e9e80ac0dd9b
salahhusa9 <salahhusa9@gmail.com>
2024-06-08 11:36:25 +0100

    Update git_path configuration

--- src/Helpers/Git.php
+++ src/Helpers/Git.php
@@ -144,20 +144,6 @@ class Git
     {
         $gitPath = config('updater.git_path');
 
-        if (! $gitPath) {
-            $gitPath = Cache::rememberForever('git_path', function () {
-                $executableFinder = new \Symfony\Component\Process\ExecutableFinder();
-
-                $gitPath = $executableFinder->find('git');
-
-                if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                    $gitPath = '"'.$gitPath.'"';
-                }
-
-                return $gitPath;
-            });
-        }
-
         return $gitPath;
     }
 }
