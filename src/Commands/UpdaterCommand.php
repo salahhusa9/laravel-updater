d506e05640568a195d759362d1e2f62795ddc869
salah eddine bendyab <salahhusa9@gmail.com>
2024-07-17 21:22:21 +0100

    Add sponsor message and link in UpdaterCommand.php

--- src/Commands/UpdaterCommand.php
+++ src/Commands/UpdaterCommand.php
@@ -28,6 +28,9 @@ class UpdaterCommand extends Command
 
         $this->components->twoColumnDetail('Application updated', Updater::getCurrentVersion());
 
+        $this->components->line('Thank you for using this package! ❤️');
+        $this->components->line('Consider supporting my work: https://github.com/sponsors/salahhusa9');
+
         return self::SUCCESS;
     }
 }
