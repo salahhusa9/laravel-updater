8c0d106fbae3f5f010224d470280a76b3605ec6d
salahhusa9 <64494826+salahhusa9@users.noreply.github.com>
2025-01-27 12:18:08 +0000

    Fix styling

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -5,16 +5,16 @@ use Salahhusa9\Updater\Helpers\Helper;
 
 class HelperTest extends TestCase
 {
-    public function testIsVersionReturnsTrueForValidVersion()
+    public function test_is_version_returns_true_for_valid_version()
     {
-        $helper = new Helper();
+        $helper = new Helper;
 
         $this->assertEquals($helper->isVersion('v1.2.3'), 1);
     }
 
-    public function testIsVersionReturnsFalseForInvalidVersion()
+    public function test_is_version_returns_false_for_invalid_version()
     {
-        $helper = new Helper();
+        $helper = new Helper;
 
         $this->assertEqualsCanonicalizing($helper->isVersion('invalid-version'), 0);
     }
